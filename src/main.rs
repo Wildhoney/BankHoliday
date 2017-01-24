48f88b85089d5e2887c57149b0b048c6bb4392ae
Wildhoney <adam.timberlake@gmail.com>
2017-01-24 23:24:30 +0000

    Added initial src and Cargo file

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+extern crate curl;
+extern crate ical;
+
+use std::io::{stdout, Write};
+use curl::easy::Easy;
+
+use std::io::BufReader;
+use std::fs::File;
+
+// Print a web page onto stdout
+fn main() {
+    let mut easy = Easy::new();
+    easy.url("https://www.gov.uk/bank-holidays/england-and-wales.ics").unwrap();
+    easy.write_function(|data| {
+        Ok(stdout().write(data).unwrap())
+    }).unwrap();
+    easy.perform().unwrap();
+
+    // println!("{}", easy.response_code().unwrap());
+
+    let reader = ical::IcalReader::new(easy.response_code().unwrap());
+
+    for line in reader {
+        println!("{:?}", line);
+    }
+
+}

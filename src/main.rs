3741925ce295cc6c7baf13101242fa9f15f0597e
Wildhoney <adam.timberlake@gmail.com>
2017-01-25 08:08:35 +0000

    Added const at top-level for url

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,17 @@ use curl::easy::Easy;
 use std::io::BufReader;
 use std::fs::File;
 
+const url: u32 = "https://www.gov.uk/bank-holidays/england-and-wales.ics";
+
 fn main() {
     let mut easy = Easy::new();
-    easy.url("https://www.gov.uk/bank-holidays/england-and-wales.ics").unwrap();
+    easy.url(url).unwrap();
     easy.write_function(|data| {
         Ok(stdout().write(data).unwrap())
     }).unwrap();
     easy.perform().unwrap();
 
-    let reader = ical::IcalReader::new(easy.response_code().unwrap());
+    let reader = ical::IcalParser::new(easy.response_code().unwrap());
 
     for line in reader {
         println!("{:?}", line);
